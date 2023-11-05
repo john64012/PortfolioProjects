c72aec4a66767c11857552a50e46ef622e92161c
john64012 <44446427+john64012@users.noreply.github.com>
2023-11-05 09:55:00 -0500

    Delete AirBnB Top Revenue Query.sql

--- AirBnB Top Revenue Query.sql
+++
@@ -1,5 +0,0 @@
-SELECT id, listing_url, name, 30 - availability_30 as booked_out_30,
-	CAST(replace(Price, '$', ' ') as UNSIGNED) as price_clean,
-	CAST(replace(Price, '$', ' ') as UNSIGNED)*(30 - availability_30) as proj_rev_30 
-FROM airbnblistings
-ORDER BY proj_rev_30 DESC LIMIT 30
