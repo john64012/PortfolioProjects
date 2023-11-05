7303c83f8c84bb6aedaf45a78799c70448d6313a
john64012 <44446427+john64012@users.noreply.github.com>
2023-11-05 09:56:16 -0500

    Update AirBnB Top Revenue Query.sql
    added context

--- Blank File/AirBnB Top Revenue Query.sql
+++ Blank File/AirBnB Top Revenue Query.sql
@@ -1,3 +1,5 @@
+-- Once unzipped, run this code to see the top 30 AirBnBs in the NYC area ordered by projected revenue for the next 30 days.
+
 SELECT id, listing_url, name, 30 - availability_30 as booked_out_30,
 	CAST(replace(Price, '$', ' ') as UNSIGNED) as price_clean,
 	CAST(replace(Price, '$', ' ') as UNSIGNED)*(30 - availability_30) as proj_rev_30 
