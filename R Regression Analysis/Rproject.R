3d26f89794ae377916bbe2e275c52eba8a1de23f
john64012 <44446427+john64012@users.noreply.github.com>
2023-11-13 13:03:56 -0500

    Update Rproject.R
    Added context

--- R Regression Analysis/Rproject.R
+++ R Regression Analysis/Rproject.R
@@ -11,6 +11,8 @@ smoker = as.factor(df$smoker)
 sex = as.factor(df$sex)
 region = as.factor(df$region)
 
+## creates boxplots for dependent variables set up against cost
+
 boxplot(charges ~ smoker, data = df, main = 'smoker')
 boxplot(charges ~ sex, data = df, main = 'sex')
 boxplot(charges ~ region, data = df, main = 'region')
@@ -20,3 +22,6 @@ model1 = lm(charges ~. , data=df)
 
 ```{r}
 summary(model1)
+
+## creates summary for dataset. With an R-squared of 0.75, we have fair confidence that the variables of smoking, age, and bmi have an impact on cost of charges.
+## being in the southwest or southeast have a smaller but noticeable impact on charges as well. one must recognize correlation vs. causation with this relationship however.
